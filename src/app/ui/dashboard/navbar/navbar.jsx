d8fcf925a74b83e50d76b678cdd9a0a29e065f33
FrancisJTR7 <xgalasko@outlook.com>
2024-05-21 19:13:14 -0400

    navbar done

--- src/app/ui/dashboard/navbar/navbar.jsx
+++ src/app/ui/dashboard/navbar/navbar.jsx
@@ -12,17 +12,23 @@ const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <div>
-      <div>{pathname.split('/').pop()}</div>
-      <div>
-        <div>
+    <div className='flex justify-between items-center p-[20px] rounded-[10px] bg-[#182237]'>
+      <div className='font-bold text-[#b7bac1] capitalize'>
+        {pathname.split('/').pop()}
+      </div>
+      <div className='flex items-center gap-[20px]'>
+        <div className='flex items-center gap-[10px] bg-[#2e374a] p-[10px] rounded-[10px]'>
           <MdSearch />
-          <input type='text' placeholder='Search...' />
+          <input
+            type='text'
+            placeholder='Search...'
+            className='bg-transparent border-none text-white'
+          />
         </div>
-        <div>
-          <MdOutlineChat />
-          <MdNotifications />
-          <MdPublic />
+        <div className='gap-[20px] flex'>
+          <MdOutlineChat size={20} />
+          <MdNotifications size={20} />
+          <MdPublic size={20} />
         </div>
       </div>
     </div>
