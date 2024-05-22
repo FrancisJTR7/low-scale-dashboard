a88e9ef91b309605ec7aadc03db01aba9db15f2a
FrancisJTR7 <xgalasko@outlook.com>
2024-05-21 21:14:31 -0400

    done for today

--- src/app/ui/dashboard/transactions/transactions.jsx
+++ src/app/ui/dashboard/transactions/transactions.jsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+
 const Transactions = () => {
   return (
-    <div>
-      <h2>Latest Transactions</h2>
-      <table>
+    <div className='bg-soft p-[20px] rounded-[10px]'>
+      <h2 className='mb-[20px] font-[200] text-tsoft'>Latest Transactions</h2>
+      <table className='w-full '>
         <thead>
           <tr>
             <td>Name</td>
@@ -11,7 +13,53 @@ const Transactions = () => {
             <td>Amount</td>
           </tr>
         </thead>
-        <tbody></tbody>
+        <tbody>
+          <tr>
+            <td>
+              <Image
+                src='/images.jpeg'
+                alt='profile'
+                width='40'
+                height='40'
+                className='rounded-[50%] object-cover'
+              />
+              John Doe
+            </td>
+            <td> pending </td>
+            <td>02.14.2024</td>
+            <td>$3,500</td>
+          </tr>
+          <tr>
+            <td>
+              <Image
+                src='/images.jpeg'
+                alt='profile'
+                width='40'
+                height='40'
+                className='rounded-[50%] object-cover'
+              />
+              John Doe
+            </td>
+            <td> pending </td>
+            <td>02.14.2024</td>
+            <td>$3,500</td>
+          </tr>
+          <tr>
+            <td>
+              <Image
+                src='/images.jpeg'
+                alt='profile'
+                width='40'
+                height='40'
+                className='rounded-[50%] object-cover'
+              />
+              John Doe
+            </td>
+            <td> pending </td>
+            <td>02.14.2024</td>
+            <td>$3,500</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
