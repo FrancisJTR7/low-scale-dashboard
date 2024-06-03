470fb34395a19be57e91eb690887e99b7f2fdf76
FrancisJTR7 <xgalasko@outlook.com>
2024-06-03 18:50:09 -0400

    KPI frontend done, imported fonts

--- src/app/ui/snapshot/kpi/kpi.jsx
+++ src/app/ui/snapshot/kpi/kpi.jsx
@@ -1,4 +1,5 @@
 import clsx from 'clsx';
+import { FaCaretUp, FaCaretDown } from 'react-icons/fa6';
 
 const kpiItems = [
   {
@@ -46,16 +47,20 @@ const Kpi = () => {
           <div className='text-[.8rem] font-extrabold text-black'>
             {item.title}
           </div>
-          <div className='flex justify-center gap-2'>
+          <div className='flex justify-center items-center gap-2'>
             <div className='text-2xl font-extrabold text-black'>
               {item.value}
             </div>
             <div
-              className={clsx('p-1  h-8 font-extrabold rounded-lg ', {
-                'text-green-500 bg-green-200': item.positive,
-                'text-red-500 bg-red-200': !item.positive,
-              })}
+              className={clsx(
+                'flex items-center px-1 h-5 font-extrabold rounded-md text-xs',
+                {
+                  'text-green-500 bg-green-200': item.positive,
+                  'text-red-500 bg-red-200': !item.positive,
+                }
+              )}
             >
+              {item.positive ? <FaCaretUp /> : <FaCaretDown />}
               {item.change}
             </div>
           </div>
